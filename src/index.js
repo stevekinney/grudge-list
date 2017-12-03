2467e985469596bdfb30c0926cb3601ce8b32bb7
Steve Kinney <hello@stevekinney.net>
2017-12-03 12:40:18 -0700

    Set up ReactOpt

--- src/index.js
+++ src/index.js
@@ -5,4 +5,9 @@ import Application from './components/Application';
 
 import './index.css';
 
+if (process.env.NODE_ENV !== 'production') {
+  const { reactopt } = require('reactopt');
+  reactopt(React);
+}
+
 ReactDOM.render(<Application />, document.getElementById('root'));
