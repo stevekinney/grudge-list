a6d1aae5dfa6abf5efe2e835b47688395e330c91
Steve Kinney <hello@stevekinney.net>
2017-12-03 11:12:31 -0700

    Get stuff onto the page

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,10 +3,36 @@ import React, { Component } from 'react';
 import initialData from '../data.json';
 
 export default class Application extends Component {
+  state = {
+    grudges: initialData
+  }
+
   render() {
+    const { grudges } = this.state;
+
     return (
       <div className="Application">
         <h1>Grudge List</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Full Name</th>
+              <th>Transgression</th>
+              <th>Forgiven?</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            { grudges.map((grudge, index) => (
+              <tr key={index}>
+                <td>{ grudge.fullName }</td>
+                <td>{ grudge.transgression }</td>
+                <td>{ grudge.forgiven ? 'Yes' : 'No' }</td>
+                <td><button>{ grudge.forgiven ? 'Unforgive' : 'Forgive' }</button></td>
+              </tr>
+            )) }
+          </tbody>
+        </table>
       </div>
     );
   }
